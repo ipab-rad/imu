9751b311125f31f86f8db337e04d5c24730508de
hect95 <hector_cruz95@live.com>
2024-05-16 13:09:37 +0100

    Avoid overriding dev and runtime images

--- dev.sh
+++ dev.sh
@@ -5,7 +5,7 @@
 
 # Build docker image up to dev stage
 DOCKER_BUILDKIT=1 docker build \
-    -t av_imu:latest \
+    -t av_imu:latest-dev \
     -f Dockerfile --target dev .
 
 # Run docker image with local code volumes for development
@@ -13,4 +13,4 @@ docker run -it --rm --net host --privileged \
     -v /dev/shm:/dev/shm \
     -v /dev/imu-6:/dev/imu-front \
     -v ./av_imu_launch:/opt/ros_ws/src/av_imu_launch \
-    av_imu:latest
+    av_imu:latest-dev
